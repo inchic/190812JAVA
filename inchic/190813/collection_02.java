9de8591a4d850ed4027c23e0be5c10c34e77acdc
inchic <poiuy1709@naver.com>
2019-08-13 12:29:00 +0900

    inchic

--- inchic/190813/collection_02.java
+++ inchic/190813/collection_02.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class collection_02 {
-    
+
     public static void main(String[] args) {
         ArrayList<String> key = new ArrayList<String>();
         key.add("Mango");
@@ -22,7 +22,7 @@ public class collection_02 {
         apple.add(20);
         apple.add(10);
         apple.add(32);
-        
+
         ArrayList<Integer> banana = new ArrayList<Integer>();
         banana.add(99);
         banana.add(23);
@@ -34,13 +34,13 @@ public class collection_02 {
         melon.add(45);
         melon.add(12);
 
-        HashMap<Object,Object> hmap = new HashMap<Object,Object>();
-        hmap.put("Key",key);
-        hmap.put("Mango",mango);
-        hmap.put("Apple",apple);
-        hmap.put("Banana",banana);
-        hmap.put("Melon",melon);
-        
+        HashMap<Object, Object> hmap = new HashMap<Object, Object>();
+        hmap.put("Key", key);
+        hmap.put("Mango", mango);
+        hmap.put("Apple", apple);
+        hmap.put("Banana", banana);
+        hmap.put("Melon", melon);
+
         ArrayList<String> list_key = new ArrayList<String>();
         ArrayList<Integer> list_mango = new ArrayList<Integer>();
         ArrayList<Integer> list_apple = new ArrayList<Integer>();
@@ -52,9 +52,43 @@ public class collection_02 {
         list_apple = (ArrayList<Integer>) hmap.get("Apple");
         list_banana = (ArrayList<Integer>) hmap.get("Banana");
         list_melon = (ArrayList<Integer>) hmap.get("Melon");
+        // 숫자가 list
+        // 망고가 map
 
-        for(string a: list_key){
-            System.out.println(a);
+        // 어레이를만들고 값에 넣어준다음에 해쉬맵에 넣어준다 x4
+
+        // mango : 39, 20, 30, 12, 40
+        // apple : 48, 20, 10, 32
+        // banana : 99, 23, 123
+        // melon : 49, 22, 45, 12
+        for (String a : list_key) {
+            System.out.printf(a + ": ");
+            switch (a) {
+            case "Mango":
+                for (int b : list_mango) {
+                    System.out.printf(b + " ");
+                }
+                System.out.println("");
+                break;
+            case "Apple":
+                for (int c : list_apple) {
+                    System.out.printf(c + " ");
+                }
+                System.out.println("");
+                break;
+            case "Banana":
+                for (int d : list_banana) {
+                    System.out.printf(d + " ");
+                }
+                System.out.println("");
+                break;
+            case "Melon":
+                for (int e : list_melon) {
+                    System.out.printf(e + " ");
+                }
+                System.out.println("");
+                break;
+            }
         }
 
     }
