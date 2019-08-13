64630f2578c2c08cf7473ec2551801681bfb1c4e
inchic <poiuy1709@naver.com>
2019-08-13 16:40:13 +0900

    inchic

--- inchic/190813/innerClass_06.java
+++ inchic/190813/innerClass_06.java
@@ -1,5 +1,10 @@
+import java.util.ArrayList;
+
 public class innerClass_06{
     public static void main(String[] args) {
-        
+        ArrayList<Fruits> mFruitsList=new ArrayList<Fruits>();
+        mFruitsList.add(new Fruits("Good Fruits"));
+        mFruitsList.add(new Fruits("Bad Fruits"));
+        System.out.printf("fruit: "+mFruitsList);
     }
 }
